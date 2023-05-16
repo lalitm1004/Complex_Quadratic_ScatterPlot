358983a9c3e0efc28ef2218bb8c1475bf9b5affa
walmartPhilosopher <lalitm1004@gmail.com>
2023-05-16 18:43:48 +0530

    script can now save individual plots

--- main.py
+++ main.py
@@ -25,19 +25,45 @@ def coordinate_generator(a, b, c, a_y, b_y):
     if solution2.success:
         solutions.append(solution2.x)
 
+    # print(solutions)
     return ((solutions[0][0], solutions[0][1]), (solutions[1][0], solutions[1][1]))
+
+
+def plot_axes():
+  axis = np.linspace(-20, 20, 2000)
+  zero = np.zeros_like(axis)
+  
+  # Plot x-axis RED
+  ax.plot3D(axis, zero, zero, 'red')
+  # Plot y-axis BLUE
+  ax.plot3D(zero, axis, zero, 'green')
+  # Plot z-axis GREEN
+  ax.plot3D(zero, zero, axis, 'blue')
+  ax.set_xlabel('x')
+  ax.set_zlabel('y')
+  ax.set_ylabel('z')
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
-
-for a_y in np.linspace(-20,20,500):
+for b_y in np.linspace(-20,20,50):
     
-  coord = coordinate_generator(1,0,25,a_y,0)
-  point1 = (coord[0][0], coord[0][1], a_y)
-  point2 = (coord[1][0], coord[1][1], a_y)
-  
-  
-  ax.scatter(point1[0],point1[1],point1[2], c='red')
-  ax.scatter(point2[0],point2[1],point2[2], c='red')
-  print(coord)
-plt.show()
+    plt.cla()
+    plot_axes()
+    
+    for a_y in np.linspace(-20,20,20):
+        
+        coord = coordinate_generator(1,0,9,a_y,b_y)
+        point1 = (coord[0][0], coord[0][1], a_y)
+        point2 = (coord[1][0], coord[1][1], a_y)
+        
+        
+        ax.scatter(point1[0],point1[1],point1[2], c='black')
+        ax.scatter(point2[0],point2[1],point2[2], c='black')
+        print(coord)
+
+    # Create a dummy line for the legend
+    dummy_line = ax.plot3D([], [], [], label=f'Complex Y = {round(b_y, ndigits=2)}')
+    ax.legend()
+
+    
+    plt.savefig(f'scatterplot{round(b_y, ndigits=2) + 20}.png'.replace('-','_'))
